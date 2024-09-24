72bfad2fad3e8389008b890fc76748521f00b9db
TheGuitarGuy <86133076+TheGuitarGuy@users.noreply.github.com>
2024-09-23 22:36:58 -0400

    package

--- solution-hw4/src/App.js
+++ solution-hw4/src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import MainGrid from './components/MainGrid';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <MainGrid />
+    </div>
   );
 }
 
