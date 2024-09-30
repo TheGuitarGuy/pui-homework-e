45605a7a95b873fad5dfb9ed89d257a928b50d71
TheGuitarGuy <86133076+TheGuitarGuy@users.noreply.github.com>
2024-09-30 16:24:06 -0400

    debugging

--- solution-hw4/src/App.js
+++ solution-hw4/src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
 import Home from './components/Home';
 import MainGrid from './components/MainGrid';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {/* Default route to the landing page */}
-          <Route path="/" element={<LandingPage />} />
-          
-          {/* Route to MainGrid */}
-          <Route path="/main-grid" element={<MainGrid />} />
-          
-          {/* Route to Home */}
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
+    <MainGrid/>
   );
 }
 
